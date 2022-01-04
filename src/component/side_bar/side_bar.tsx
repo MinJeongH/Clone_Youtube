cf936d380307d53cd9fc2dc7042a56355ca986ef
MinJengH <gkaalswjd2@gmail.com>
2022-01-04 14:23:35 +0900

    make side bar form

--- src/component/side_bar/side_bar.tsx
+++ src/component/side_bar/side_bar.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React from "react";
+import styles from "./side_bar.module.css";
+import { faHome, faCompass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const SideBar = () => <h1>Side Bar</h1>
-
+const SideBar = () => {
+  return (
+    <section className={styles.sidebar}>
+      <button className={styles.button}>
+        <FontAwesomeIcon className={styles.home} icon={faHome} size="2x" />
+        Home
+      </button>
+      <button className={styles.button}>
+        <FontAwesomeIcon
+          className={styles.compass}
+          icon={faCompass}
+          size="2x"
+        />
+        Research
+      </button>
+      <button className={styles.button}>
+        <img
+          className={styles.sub}
+          src="/images/subscribe.png"
+          alt="subscribe"
+        />
+        Subscribe
+      </button>
+      <button className={styles.button}>
+        <img className={styles.rocker} src="/images/rocker.png" alt="rocker" />
+        Rocker
+      </button>
+    </section>
+  );
+};
 export default SideBar;
